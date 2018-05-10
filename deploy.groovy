0a403a6ef1660d1ede799b26babf1a9ae0438a86
Josh Minzner <josh@trialspark.com>
2018-05-10 11:17:25 -0400

    add jenkins deploy script

---
+++ deploy.groovy
@@ -0,0 +1,17 @@
+node {
+  checkout scm
+
+  stage('Set up remote') {
+    def remoteURL = 'git@beta.aptible.com:spark-prod/github-review-slack-notifier.git'
+
+    try {
+      sh "git remote add aptible ${remoteURL}"
+    } catch (all) {
+      sh "git remote set-url aptible ${remoteURL}"
+    }
+  }
+
+  stage('Push to aptible') {
+    sh "git push aptible ${env.BRANCH_NAME}"
+  }
+}
